c733367fa591d55dd9d46d44b448750e67e300b5
Ali Mert Çakar <mail@alimert.net>
2023-02-19 21:15:44 +0300

    code

--- server/index.ts
+++ server/index.ts
@@ -204,6 +204,6 @@ function getRoomFromLink(link: string): Room | undefined {
     return id !== undefined ? roomsCache.get(id) : undefined
 }
 
-httpServer.listen(process.env.PORT || 80, () => {
-    console.log('listening on port', process.env.PORT || 80)
+httpServer.listen(process.env.PORT || 5001, () => {
+    console.log('listening on port', process.env.PORT || 5001)
 })
